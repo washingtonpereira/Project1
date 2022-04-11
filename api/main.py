6d64e1fa44ccf05248652b44961bfa9e25aca720
washington Pereira <slash.o.teobaldo@gmail.com>
2022-04-11 13:30:59 -0300

    Get e Post inclusos

--- api/main.py
+++ api/main.py
@@ -1,11 +1,35 @@
-from typing import Optional
+from typing import List , Optional
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from uuid import uuid4
 
 app = FastAPI()
 
-@app.get("/")
-def reload():
-    return {"Hello":"World"}
+class Animal(BaseModel):
+    id: Optional[int]
+    nome: str
+    sexo: str
+    cor: str
+    
+banco: List[Animal] =[] 
+
+@app.get('/')
+def home():
+    return {"Seja bem vindo(a)":"Washington"}
+
+@app.get('/animais')
+def listar_animal():
+    return banco
+
+@app.post('/animais')
+def criar_animal(animal: Animal):
+    animal.id = uuid4()
+    banco.append(animal)
+    return None
+
+
+
+
+
 
 
